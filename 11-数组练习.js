94f031bed3d3917830e0233156e0f45193756faa
longdandan-dev <2596429468@qq.com>
2026-09-12 20:03:42 +0800

    阶段0模块1：对象与数组方法跟练本 + 歌单数据台练习 + 批改规则

--- 11-数组练习.js
+++ 11-数组练习.js
@@ -1,4 +1,4 @@
-/* ============================================================
+ /* ============================================================
    学习日 5 · 独立练习：数组入门
    ------------------------------------------------------------
    规则不变：
@@ -15,8 +15,11 @@
 // 提示：长度是 songs.length；第一首是 songs[0]；索引从 0 开始，最后一首是 songs[4]
 
 // TODO: 在这里写你的代码
-
-
+const songs = ["晴天","多远都要在一起","烟火里的尘埃","喜欢你","告白气球"]
+console.log("1) 长度是", songs.length);
+console.log("1) 第一首", songs[0]);
+console.log("1) 第三首", songs[2]);
+console.log("1) 最后一首", songs[songs.length - 1]);
 
 // ===== 练习 2：push 和 pop（数组两头）=====
 // 要求：用 push 往歌单末尾加一首歌，再用 pop 把末尾那首弹出来
@@ -25,9 +28,18 @@
 // 提示：push 和 pop 都会改变原数组；pop 会把弹出的那首"交出来"，可以用变量接住
 
 // TODO: 在这里写你的代码
+songs.push("简单爱");
+console.log("2) push之后",songs.length,"首，最后一首是",songs[songs.length -1])
 
+const out = songs.pop();
+console.log("2) pop弹出了",out,"，现在剩",songs.length,"首");
 
+songs.unshift("屋顶");
+console.log("2) unshift之后第一首是",songs[0],"一共",songs.length,"首")
 
+const first= songs.shift();
+console.log("2) shift拿走了",first
+,"，现在第一首是",songs[0]);
 // ===== 练习 3：用 for 循环遍历数组 =====
 // 要求：用 for 循环把 songs 里每一首都打印出来，格式："3) 第 1 首：晴天"
 //       循环结束后再打印一行，以 "3)" 开头，说明一共有几首
@@ -39,8 +51,10 @@
 // 提示：序号写 i + 1，歌名写 songs[i]，循环条件用 i < songs.length
 
 // TODO: 在这里写你的代码
-
-
+for (let i = 0;i < songs.length;i++){
+   console.log(" 3) 第",i + 1,"首：",songs[i]);
+}
+console.log("3) 一共",songs.length,"首");
 
 // ===== 练习 4：数字数组求和 + 找最大值 =====
 // 要求：声明 nums = [12, 5, 33, 8, 21]
@@ -50,8 +64,17 @@
 // 提示：和先设 let sum = 0；最大值先设 let max = nums[0]，再一个一个比大小
 
 // TODO: 在这里写你的代码
+const nums = [12,5,33,8,21];
+let sum = 0;
+let max = nums[0];
 
-
+for (let i = 0;i < nums.length;i++){
+   sum = sum + nums[i];
+   if (nums[i] >max){
+      max = nums[i];
+   }
+}
+console.log("4) 和是",sum ,"，最大值",max);
 
 // ===== 练习 5：倒着遍历数组 =====
 // 要求：声明 arr = [1, 2, 3, 4, 5]
@@ -61,9 +84,12 @@
 // 提示：先 let result = ""；循环里 result = result + arr[i]；最后一位下标是 arr.length - 1
 
 // TODO: 在这里写你的代码
-
-
-
+const arr = [1,2,3,4,5];
+let result = "";
+for(let i = arr.length - 1;i>=0;i--){
+   result = result + arr[i];
+}
+console.log("5) 倒着是",result,"，一共",arr.length,"个数字")
 /* ===== 写完之后 =====
    1. 终端运行：node 检查.js
    2. 结果发我
